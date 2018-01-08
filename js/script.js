8c7dbd75a1b487aabcce97c3e17ef854b6b5e31f
Ravern Koh <ravern.koh.dev@gmail.com>
2018-01-08 18:15:21 +0800

    Added navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+// Keep everything within this function.
 window.onload = function() {
     // Include the header HTML.
-    w3.includeHTML();
+    w3.includeHTML(function() {
+        // Dropdown menu.
+        $("#dropdown-container").hover(function() {
+            $("#dropdown-content").addClass("show");
+            $("#dropdown").css("background-color", "#f73e4d");
+        }, function() {
+            $("#dropdown-content").removeClass("show");
+            $("#dropdown").css("background-color", "#ed2939");
+        });
+    });
 };
